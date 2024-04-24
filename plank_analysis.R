dd749e1b446fffd6c5f8d808453ddb86092cf644
Louis Cerda <93852998+louisdcerda@users.noreply.github.com>
2024-04-24 21:20:38 +0200

    modified data and added graphs/correlations

--- plank_analysis.R
+++ plank_analysis.R
@@ -19,17 +19,60 @@ boxplot(male,female,
 
 
 # age groups mean bar graph 
+hash <- list()
 
-barplot(age_counts, 
-        names.arg = ,
+  # splitting data up by 5 incrs 
+for (i in plank_data$Age) {
+  age_group <- floor(i / 5) * 5
+  if (!age_group %in% names(hash)) {
+    hash[[as.character(age_group)]] <- 1
+  } else {
+    hash[[as.character(age_group)]] <- hash[[as.character(age_group)]] + 1
+  }
+}
+age_labels = c("10-14","15-19","20-24","30-34", "45-49","50-54")
+
+barplot(unlist(hash), 
+        names.arg = age_labels,
         main = "Number of individuals by Age Group (step = 5)",
         xlab = "Age Group",
         ylab = "Number of Individuals")
 
+
+# analyze only those in there 20s
+
+select_few <- (plank_data[floor(plank_data$Age /5 ) == 4, 'Time'])$Time
+
 # age groups graph - mean by 5's
+
+
 
 # told vs untold - correlation?
 
+
+
+
 # age group and times two lines by gender
 
-# 
+
+
+
+
+# linear regression
+
+  # dept var = time
+  # indep var = gender, age, via 
+  # 
+  # encode gender and via into 1s and 0s for numberical values
+  # preform lineer reg
+  # analyze coef? to understand the impact of each indep var on plank dur
+
+lin_reg_data = read_excel('../final_project/stats_proj_data.xlsx')
+lin_reg_data$Gender <- as.numeric((factor(lin_reg_data$Gender)))
+lin_reg_data$Via <- as.numeric((factor(lin_reg_data$Via)))
+
+linReg_model <- lm(Time ~ Gender+Age, data = lin_reg_data)
+
+summary(linReg_model)
+
+
